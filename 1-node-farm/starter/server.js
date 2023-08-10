9466d9367a26e01e2d01430a72f7af6f97f106f7
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-08-11 02:25:24 +0530

    commit :: import export module

--- 1-node-farm/starter/server.js
+++ 1-node-farm/starter/server.js
@@ -1,37 +1,29 @@
 const http = require("http");
 const fs = require("fs");
-const url = require('url')
+const url = require('url');
+const slugify =  require('slugify');
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
+const replaceTemplate = require('./module/replaceTemplate')
 
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
-);
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  "utf-8"
-);
-const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  "utf-8"
-);
-
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
-const dataObj = JSON.parse(data);
-
+  );
+  const tempCard = fs.readFileSync(
+    `${__dirname}/templates/template-card.html`,
+    "utf-8"
+    );
+    const tempProduct = fs.readFileSync(
+      `${__dirname}/templates/template-product.html`,
+      "utf-8"
+      );
+      
+      const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+      const dataObj = JSON.parse(data);
+      const slugs = dataObj.map(el => slugify(el.productName , {lower : true} ))
+      console.log(slugs);
+      
 const server = http.createServer((req, res) => {
   const {pathname , query} = url.parse(req.url,true);
   /*Overview Page */
