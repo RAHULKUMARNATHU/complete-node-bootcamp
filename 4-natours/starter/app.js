35d0d4c6db91393a926c65bad12a47497e4d86c6
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-09-18 08:57:41 +0530

    commit :: get-all and create natours

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,17 +1,56 @@
+const fs = require('fs'); /*Core module */
 const express = require('express');
 
 const app = express();
 
+/*Here express.json is middleware */
+app.use(express.json());
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello  from the server side! ', app: 'Natours' });
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello  from the server side! ', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.status(201).json({ message: 'You can post this endpoint ' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+); /*JSON.parse ----> converts into javascript object */
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-  res.status(201).json({ message: 'You can post this endpoint ' });
+app.post('/api/v1/tour', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+
+  newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 201,
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
