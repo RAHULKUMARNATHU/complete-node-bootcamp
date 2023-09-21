f407f496418baf0c963048407eb6293f52be583d
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-09-21 10:05:35 +0530

    commit :: modified tour model

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -3,17 +3,55 @@ const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
   name: {
     type: String,
+    trim: true,
     required: [true, 'A tour must have a name'],
     unique: [true, 'The tour name must be unique'],
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
     type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
+    type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  discount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover:{
+    type:String,
+    required:[true ,'A tour must have a cover images']
+  },
+  images:[String],
+  createdAt:{
+    type:Date,
+    default:Date.now()
+  },
+  startDates:[Date],
+  
 });
 
 module.exports = mongoose.model('Tour', tourSchema);
