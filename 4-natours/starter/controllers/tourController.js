7f76c87733e219a72f9e49c35623fada37cef87a
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-09-22 18:59:19 +0530

    commit :: refactor the code to make reusable for sort ,limit , paginate and filter

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('./../models/tourModel');
+const APIFeatures = require('./../utils/apiFeatures');
 
 /*getAll tours */
 
@@ -11,63 +12,15 @@ exports.aliasTopTours = async (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    //BUILD QUERY
-    //1A) Filtering
-    const queryObj = { ...req.query };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-    // console.log(queryObj, req.query);
-    // console.log(req.requestTime);
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
 
-    /*1B) ADVANCE FILTERING */
-    let queryStr = JSON.stringify(queryObj);
-
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // console.log(queryStr);
-    /*{difficulty:'easy' , duration:{ $gte: 5}}
-    we have achieved {"difficulty":"easy","duration":{"$gte":"5"}}
-    after JSON.parse we get the exact 
-  ` query URL http://localhost:8000/api/v1/tours?duration[gte]=5&difficulty=easy&page=2&sort=1&limit=10&price[lte]=1500*/
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    /*2)SORTING */
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    /*3) Field limiting */
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    /*4)PAGINATION */
-    const page = req.query.page * 1 || 1;
-    console.log(req.query.page);
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTour = await Tour.countDocuments();
-      console.log(skip);
-      if (skip >= numTour) {
-        throw new Error('This page does not exists');
-      }
-    }
-
-    const tours = await query;
-
-    /*ANOTHER WAY TO FILTER */
-    /* query.sort().select().skip().limit() */
-    // const query = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
+    // console.log(await features.query);
+    // const tours = await query; /*previously */
+    const tours = await features.query;
 
     res.status(200).json({
       status: 'success',
