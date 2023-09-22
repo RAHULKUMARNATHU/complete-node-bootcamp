21c0c5686436623757dbd177130172722de9a2a8
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-09-22 14:44:06 +0530

    commit :: pagination , limit , sort ,fields limit

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -5,14 +5,14 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     //BUILD QUERY
-    //1) Filtering
+    //1A) Filtering
     const queryObj = { ...req.query };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach((el) => delete queryObj[el]);
     // console.log(queryObj, req.query);
     // console.log(req.requestTime);
 
-    /*ADVANCE FILTERING */
+    /*1B) ADVANCE FILTERING */
     let queryStr = JSON.stringify(queryObj);
 
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
@@ -22,13 +22,45 @@ exports.getAllTours = async (req, res) => {
     after JSON.parse we get the exact 
   ` query URL http://localhost:8000/api/v1/tours?duration[gte]=5&difficulty=easy&page=2&sort=1&limit=10&price[lte]=1500*/
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    /*2)SORTING */
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    /*3) Field limiting */
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    /*4)PAGINATION */
+    const page = req.query.page * 1 || 1;
+    console.log(req.query.page);
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTour = await Tour.countDocuments();
+      console.log(skip);
+      if (skip >= numTour) {
+        throw new Error('This page does not exists');
+      }
+    }
 
     const tours = await query;
 
     /*ANOTHER WAY TO FILTER */
-
-    // const query = await Tour.find().where('duration').equals(5).where().equals();
+    /* query.sort().select().skip().limit() */
+    // const query = await Tour.find().where('duration').equals(5).where('difficulty').equals('easy');
 
     res.status(200).json({
       status: 'success',
