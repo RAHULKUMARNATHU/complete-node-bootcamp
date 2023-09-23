58b8dc846735cf337d5b586343fc891c67623cb3
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-09-23 23:48:14 +0530

    commit :: document middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: [true, 'The tour name must be unique'],
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -63,4 +65,21 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+/*Document middleware -- Runs before the .save() and .create()*/
+
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.post('save', function (doc, next) {
+  console.log(doc);
+  next();
+});
 module.exports = mongoose.model('Tour', tourSchema);
+
+/*Query middleware */
+
+/*Aggregate middleware*/
+
+/*Model middleware */
