f55d2499d2985500794525dac4ed89c5079b26fb
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-09-27 17:16:46 +0530

    commit  :: uncaught exception

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION!  💥 Shutting down... ');
+  console.log(err.name, ':', err.message);
+   process.exit(1);
+});
+
 const app = require('./app');
 
 // const DB = process.env.DATABASE.replace(
 //   '<PASSWORD>',
 //   process.env.DATABASE_PASSWORD,
 // );
-
+console.log(xz);
 const DB = process.env.DATABASE_LOCAL;
 mongoose
   .connect(DB, {
@@ -44,6 +51,7 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION 💥 ');
   console.log(err.name, ':', err.message);
   /*close gracefully not abrupt the running server or pending request */
   server.close(() => {
