78eaf7bf7e1bf5574963eb76f31c29d1cae9124e
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-10-02 17:12:42 +0530

    commit :: applied virtual property to tourSchema for find review of every tour

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -116,6 +116,13 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+/*virtual populate */
+tourSchema.virtual('reviews',{
+  ref:'Review',
+  foreignField:'tour',
+  localField:'_id'
+})
+
 /*Document middleware -- Runs only before the .save() and .create()*/
 
 tourSchema.pre('save', function (next) {
