c088a707737ecdfbe34e83be325faf7a563c11dc
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-10-04 01:17:59 +0530

    commit :: resolve duplicate reviews

--- 4-natours/starter/models/reviewModel.js
+++ 4-natours/starter/models/reviewModel.js
@@ -34,6 +34,7 @@ const reviewSchema = new mongoose.Schema(
   },
 );
 
+/*One user can create only one review */
 reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
 
 reviewSchema.pre(/^find/, function (next) {
