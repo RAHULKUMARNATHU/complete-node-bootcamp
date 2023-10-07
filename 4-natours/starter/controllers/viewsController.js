b4fcfe689bbcdeffea9c135e043e91f145bfb5d1
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-10-07 17:54:12 +0530

    commit :: implemented tour details page :: remains :: tour map

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,9 +1,9 @@
 const catchAsync = require('../utils/catchAsync');
-const tour = require('./../models/tourModel');
+const Tour = require('./../models/tourModel');
 
 exports.getOverview = catchAsync(async (req, res) => {
   /*1) Get tour data from collection */
-  const tours = await tour.find();
+  const tours = await Tour.find();
   /*2) Build template */
 
   /*3) Render that template using tour 1)*/
@@ -13,8 +13,15 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+
+  console.log(tour);
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour
   });
-};
+});
