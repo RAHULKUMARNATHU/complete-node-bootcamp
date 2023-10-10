e558878a9c99d57c9a24698e8a574bb620fc066d
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-10-10 23:02:15 +0530

    commit :: implemented :: proper error message

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -1,6 +1,6 @@
 const catchAsync = require('../utils/catchAsync');
 const Tour = require('./../models/tourModel');
-
+const AppError = require('./../utils/appError');
 exports.getOverview = catchAsync(async (req, res) => {
   /*1) Get tour data from collection */
   const tours = await Tour.find();
@@ -13,11 +13,13 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res , next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
   });
+
+  if (!tour) next(new AppError('There is no tour with that name. ', 404));
 
   res
     .status(200)
