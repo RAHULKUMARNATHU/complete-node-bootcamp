8de171fa375938d20c37176da9c2d543df211fad
Rahul Kumar <58096367+RAHULKUMARNATHU@users.noreply.github.com>
2023-10-11 12:44:37 +0530

    Update login.js

--- 4-natours/starter/public/js/login.js
+++ 4-natours/starter/public/js/login.js
@@ -6,7 +6,7 @@ export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://localhost:8000/api/v1/users/login',
+      url: 'https://natours-api-o2hy.onrender.com/api/v1/users/login',
       data: {
         email,
         password,
