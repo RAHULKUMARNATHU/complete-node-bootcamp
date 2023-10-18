595042a425b2ce94b208a1628975a884eb68ffaa
RAHUL KUMAR <rahulvaidya148@gmail.com>
2023-10-18 22:47:40 +0530

    commit :: use sendGrid for prod

--- 4-natours/starter/utils/email.js
+++ 4-natours/starter/utils/email.js
@@ -4,17 +4,31 @@ const htmlToText = require('html-to-text');
 
 // new Email(user ,url).sendWelcome
 module.exports = class Email {
+  
   constructor(user, url) {
     this.to = user.email;
     this.firstName = user.name.split(' ')[0];
     this.url = url;
     this.from = `Rahul Kumar <${process.env.EMAIL_FROM}>`;
+    
   }
+
+ 
+
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
       //sendgrid
-      return 1;
+      return nodemailer.createTransport({
+        // service: 'SendGrid',
+        host: 'smtp.sendgrid.net',
+        port: 587,
+        requireTLS: true,
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
     }
     /*1) Create a transport */
     return nodemailer.createTransport({
@@ -30,9 +44,9 @@ module.exports = class Email {
   }
 
   //send the actual email
- async send(template, subject) {
+  async send(template, subject) {
     //1)Render HTML based on a pug template
-    
+
     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
       firstName: this.firstName,
       url: this.url,
@@ -43,6 +57,7 @@ module.exports = class Email {
 
     const mailOptions = {
       from: this.from,
+      // from: process.env.SENDGRID_EMAIL_FROM,
       to: this.to,
       subject,
       text: htmlToText.convert(html),
@@ -57,10 +72,12 @@ module.exports = class Email {
     await this.send('welcome', 'Welcome to the Natours Family');
   }
 
-  async sendPasswordReset(){
-    await this.send('passwordReset' , 'Your password token (valid for only 10 minutes)')
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password token (valid for only 10 minutes)',
+    );
   }
+
+   
 };
-
-
- 
